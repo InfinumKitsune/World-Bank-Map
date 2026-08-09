0095e86ff3615a422dd5e9ec370357db8ab2802e
Raymond Fox <rfox88@wgu.edu>
2026-08-09 13:05:09 -0400

    RVM2 Task 1: Interactive Map: Step C commit.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('WorldBankSGV');
+
+  countries: any[] = [];
+
+  ngOnInit() {
+    fetch('https://api.worldbank.org/v2/country?format=json')
+      .then(res => res.json())
+      .then(data => {
+        this.countries = data[1].map((c: any) => ({
+          name: c.name,
+          capital: c.capitalCity,
+          region: c.region.value,
+          income: c.incomeLevel.value,
+          iso: c.iso2Code,
+          longitude: c.longitude,
+          latitude: c.latitude
+        }));
+      });
+  }
 }
